0154466be8cbc75d61ebdab8894f29a80d7ed099
MarwaZabara <marwa_zabara@yahoo.com>
2018-09-30 18:13:30 +0200

    initial commit

---
+++ Pixel-city/Pixel-city/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Pixel-city
+//
+//  Created by Marwa Zabara on 9/30/18.
+//  Copyright © 2018 Marwa Zabara. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
